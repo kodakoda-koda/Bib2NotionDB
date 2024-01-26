cdfdbc20c74d11c9f613d62a62ab5ee3434d3233
Souhei Kodama <1419033@ed.tus.ac.jp>
2024-01-26 16:59:41 +0900

    checkpoint

--- notion_scholar/run.py
+++ notion_scholar/run.py
@@ -1,13 +1,16 @@
-from typing import List
-from typing import Optional
+from typing import List, Optional
 
 from bibtexparser.bibdatabase import BibDatabase
 
-from notion_scholar.bibtex import get_bib_database_from_file
-from notion_scholar.bibtex import get_bib_database_from_string
-from notion_scholar.bibtex import get_publication_list
-from notion_scholar.notion_api import add_publications_to_database
-from notion_scholar.notion_api import get_publication_key_list_from_database
+from notion_scholar.bibtex import (
+    get_bib_database_from_file,
+    get_bib_database_from_string,
+    get_publication_list,
+)
+from notion_scholar.notion_api import (
+    add_publications_to_database,
+    get_publication_key_list_from_database,
+)
 from notion_scholar.publication import Publication
 from notion_scholar.utilities import NotionScholarException
 
@@ -17,10 +20,10 @@ class IllegalArgumentException(NotionScholarException):
 
 
 def run(
-        token: str,
-        database_id: str,
-        bib_file_path: Optional[str] = None,
-        bib_string: Optional[str] = None,
+    token: str,
+    database_id: str,
+    bib_file_path: Optional[str] = None,
+    bib_string: Optional[str] = None,
 ) -> int:
     if bib_string is not None:
         bib_database: BibDatabase = get_bib_database_from_string(string=bib_string)
@@ -37,7 +40,10 @@ def run(
         database_id=database_id,
     )
 
+    print(key_list)
+
     publication_list_filtered = [p for p in publication_list if p.key not in key_list]  # noqa: E501
+    print(publication_list_filtered)
     add_publications_to_database(
         publications=publication_list_filtered,
         token=token,
@@ -45,6 +51,6 @@ def run(
     )
 
     if not publication_list_filtered and publication_list:
-        print('\nAll the publications are already present in the database.')
+        print("\nAll the publications are already present in the database.")
 
     return 0
